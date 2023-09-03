ff0c2fb6a74f4049ea04fb1d32b0d5179d38cd90
Dario Cvitanović <41194638+TRDario@users.noreply.github.com>
2023-09-03 19:20:41 +0200

    Add files via upload

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "id": "trdth",
+    "title": "TRDario",
+    "contact": "therealdario123@gmail.com",
+    "servers": [
+        "https://mirrors.thpatch.net/trdth/"
+    ],
+    "patches": {
+        "unvertical": "Flips vertical text to display horizontally."
+    }
+}
