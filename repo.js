a6db94adb5a0bd375ac9eeb71529f6b46460724f
Dario Cvitanović <41194638+TRDario@users.noreply.github.com>
2023-09-03 19:45:08 +0200

    Update repo.js

--- repo.js
+++ repo.js
@@ -3,7 +3,7 @@
     "title": "TRDario's patches",
     "contact": "therealdario123@gmail.com",
     "servers": [
-        "https://mirrors.thpatch.net/trdth/"
+        "https://mirrors.thpatch.net/TRDario/"
     ],
     "patches": {
         "unvertical": "Flips vertical text to display horizontally."
